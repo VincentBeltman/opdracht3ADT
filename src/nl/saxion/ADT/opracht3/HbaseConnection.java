66d7eb907f358311148ea51984d7e464bf6f12bc
mikeholtkamp <327450@student.saxion.nl>
2015-03-26 05:30:19 -0400

    apl + singleton

--- src/nl/saxion/ADT/opracht3/HbaseConnection.java
+++ src/nl/saxion/ADT/opracht3/HbaseConnection.java
@@ -8,12 +8,26 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.MasterNotRunningException;
+import org.apache.hadoop.hbase.ZooKeeperConnectionException;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.util.Bytes;
 
 public class HbaseConnection {
-	public static void main(String[] args) throws IOException {
+	
+	private static HbaseConnection instance;
+	public static HbaseConnection getInstance() throws IOException
+	{
+		if(instance == null)
+		{
+			instance = new HbaseConnection();
+		}
+		return instance;
+	}
+	
+	private HbaseConnection() throws IOException
+	{
 		Configuration config = HBaseConfiguration.create();
 
 		HBaseAdmin admin = new HBaseAdmin(config);
